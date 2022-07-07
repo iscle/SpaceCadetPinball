9da90735143f7b9462f3531a7e4e432a95aacd1f
Federico Matteoni <fedex15@live.it>
2022-07-07 23:22:08 +0200

    Handle cheats in highscore registration

--- app/src/main/java/com/fexed/spacecadetpinball/HighScoreHandler.java
+++ app/src/main/java/com/fexed/spacecadetpinball/HighScoreHandler.java
@@ -7,11 +7,14 @@ public class HighScoreHandler {
     static boolean postHighScore(Context context, int score) {
         SharedPreferences prefs = context.getSharedPreferences("com.fexed.spacecadetpinball", Context.MODE_PRIVATE);
 
-        int oldscore = prefs.getInt("highscore", 0);
-        if (score > oldscore) {
-            prefs.edit().putInt("highscore", score).apply();
-            return true;
+        if (!prefs.getBoolean("cheatsused", true)) {
+            int oldscore = prefs.getInt("highscore", 0);
+            if (score > oldscore) {
+                prefs.edit().putInt("highscore", score).apply();
+                return true;
+            }
         }
+
         return false;
     }
 
